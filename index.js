33d5abc3c7f0a76639b7a878345f0a7947f5fb1f
Emily Davidson <emilyabigaildavidson@gmail.com>
2022-08-08 09:20:16 -0400

    final

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { networkInterfaces } = require("os");
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -9,14 +10,51 @@ function ask(questionText) {
 
 start();
 
+//start by asking participant to play a game
 async function start() {
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
   );
+  //ask for the secret number
   let secretNumber = await ask(
     "What is your secret number?\nI won't peek, I promise...\n"
   );
-  console.log("You entered: " + secretNumber);
-  // Now try and complete the program.
+  //particiapant enters number
+  console.log("You entered: " + secretNumber + "\nLet's play!");
+  //computer guesses random generated response:
+  let firstGuess = Math.floor(Math.random() * 101);
+  console.log("Is your number " + firstGuess + "?");
+  //computer waits for response and askes y/n
+  let yesNo = await ask("Yes or No?\n");
+  ß;
+  //capture yes or no response
+
+  //!no is not working? (YES IS WORKING?)
+  function resp1() {
+    const answer = "y";
+    if (answer === "y") {
+      console.log("Great!\nI guessed your number!");
+      //work on no response ITS NOT WORKING YET
+    } else {
+      console.log(
+        "OK, no biggie, is your number higher or lower than " +
+          firstGuess +
+          " ?"
+      );
+    }
+  }
+  resp1();
+
+  //leave this at the very bottom:
   process.exit();
 }
+
+/* Your number is between //display the two outer number range of the remaining options
+
+Is it… //display half of the displayed number from the options from above
+
+Yay! Your number is ! Good Game!
+I guessed in “” tries!
+
+Would you like to play again? (Y)es or (N)o?
+*/
